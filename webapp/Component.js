8f7c8cde02f7cdf7e4eb08e99234b0edfb74708d
DasProdyut <prodyutdas2208@gmail.com>
2021-11-17 16:46:35 +0000

    “m SAPUI5 CF first commitâ

---
+++ webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function(AppComponent) {
+    return AppComponent.extend("com.gyansys.bookinganalysis.Component", {
+        metadata: {
+            manifest: "json"
+        }
+    });
+});
